477ae03dfec1e67ea0ebabeb71bf0fcd451e6f7e
MHDante <mhdcj@hotmail.com>
2019-02-16 14:58:52 -0500

    Added motion tracked bat, and baseball pitching.

---
+++ Assets/BallPool.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BallPool : MonoBehaviour
+{
+    public GameObject Prefab;
+    public int PrewarmAmount;
+    private List<Ball> balls = new List<Ball>();
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+        for (int i = 0; i < PrewarmAmount; i++)
+        {
+            balls.Add(MakeBall());
+        }
+        
+    }
+
+    public Ball GetBall()
+    {
+        var b =  balls.Count > 0 ? balls[0] : MakeBall();
+        b.gameObject.SetActive(true);
+        return b;
+    }
+
+    public void ReturnBall(Ball b)
+    {
+        b.gameObject.SetActive(false);
+        var t = b.transform;
+        t.localPosition = Vector3.zero;
+        t.localRotation = Quaternion.identity;
+        t.localScale = Vector3.one;
+        balls.Add(b);
+    }
+
+    private Ball MakeBall()
+    {
+
+        var ball = Instantiate(Prefab).AddComponent<Ball>();
+        ball.pool = this;
+        ball.transform.SetParent(transform);
+        ball.gameObject.SetActive(false);
+        return ball;
+    }
+}
+
+public class Ball : MonoBehaviour
+{
+    public BallPool pool;
+    public Rigidbody rigidBody;
+    
+
+    public void ReturnToPool()
+    {
+        pool.ReturnBall(this);
+    }
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+}
---
+++ Assets/Bat.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat : MonoBehaviour
+{
+    public Transform target;
+    public Transform centerOfMass;
+    private new Rigidbody rigidbody;
+
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        Application.targetFrameRate = 90;
+        rigidbody.maxAngularVelocity = float.PositiveInfinity;
+        rigidbody.centerOfMass = transform.InverseTransformPoint(centerOfMass.position);
+    }
+
+    private void FixedUpdate()
+    {
+        var speed = 1 / Time.fixedDeltaTime;
+        rigidbody.velocity = (target.transform.position - rigidbody.position) * speed;
+
+        var delta = target.transform.rotation * Quaternion.Inverse(rigidbody.rotation);
+
+        delta.ToAngleAxis(out var angle, out var axis);
+
+        if (angle < -180) angle += 360;
+        if (float.IsInfinity(axis.x)) return;
+
+        rigidbody.angularVelocity = (Mathf.Deg2Rad * angle * speed) * axis.normalized;
+
+
+    }
+
+    private void Update()
+    {   
+        Time.fixedDeltaTime = 1f / (Application.targetFrameRate / Time.timeScale);
+    }
+}
